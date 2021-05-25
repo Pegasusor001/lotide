eeced6311d7d93a213c16291f075c89b561d9837
Pegasusor001 <linus.ji.001@gmail.com>
2021-05-24 17:40:11 -0700

    head

---
+++ tail.js
@@ -0,0 +1,44 @@
+// Function tail
+const tail = function (array) {
+  console.log(array.slice(1));
+  return array.slice(1);
+};
+
+
+// Function head
+const head = function (array) {
+  return array[0];
+};
+
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  let semiCommaA = '';
+  let semiCommaB = '';
+  
+  if (typeof(actual) === 'string') {
+    semiCommaA = '"';
+  } else {
+    semiCommaA = '';
+  }
+
+  if (typeof(expected) === 'string') {
+    semiCommaB = '"';
+  } else {
+    semiCommaB = '';
+  }
+  
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${semiCommaA}${actual}${semiCommaA} === ${semiCommaB}${expected}${semiCommaB}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${semiCommaA}${actual}${semiCommaA} !== ${semiCommaB}${expected}${semiCommaB}`);
+  }
+
+};
+
+// TEST CODE
+// assertEqual("Lighthouse Labs", "Bootcamp");
+// assertEqual(1, 1);
+
+// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
+tail([1,2,3,4])
