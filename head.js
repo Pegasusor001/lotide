5e4e893da50cbbd39861d22f61f60f3ccdfbb565
Pegasusor001 <linus.ji.001@gmail.com>
2021-05-24 17:17:51 -0700

    head

--- head.js
+++ head.js
@@ -1,6 +1,5 @@
 // Function head
 const head = function (array) {
-  console.log(array[0]);
   return array[0];
 };
 
@@ -30,7 +29,7 @@ const assertEqual = function(actual, expected) {
 };
 
 // TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+// assertEqual("Lighthouse Labs", "Bootcamp");
+// assertEqual(1, 1);
 
-head([])
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
