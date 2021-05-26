7440cde425532f489624f1446c334297cc52e5b3
Pegasusor001 <linus.ji.001@gmail.com>
2021-05-26 16:24:35 -0700

    letterPosition finished

---
+++ letterPositions.js
@@ -0,0 +1,18 @@
+const letterPositions = function(sentence) {
+  const result = {};
+
+  // logic to update results here
+  for (let i = 0; i < sentence.length; i++) {
+    if (result[sentence[i]]) {
+      result[sentence[i]].push(i)
+    } else if (sentence[i] === ' ') {
+      continue;
+    } else {
+      result[sentence[i]] = [i];
+    }
+  }
+  return result;
+};
+
+console.log(letterPositions('aabc bdd'))
+// console.log(' ', typeof ' ', ' ' === ' ')
