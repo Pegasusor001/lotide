9c38d42136df8bd3004c60dda87aa4c8bedfd323
Pegasusor001 <linus.ji.001@gmail.com>
2021-05-26 15:40:26 -0700

    version 1 to be refrator

---
+++ countLetters.js
@@ -0,0 +1,15 @@
+const countLetters = function(sentense) {
+  sentense = sentense.split('');
+  let result = {};
+
+  for (let letter1 of sentense) {
+    result[letter1] = 0;
+    for (let letter2 of sentense) {
+      if (letter1 === letter2)
+        result[letter1] ++
+    }
+  }
+  return result
+}
+
+console.log(countLetters('LHLHJJ'))
