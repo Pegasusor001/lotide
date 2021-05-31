66b2e26ed4f43dcac07fdfe6c5553625a0255bb8
Pegasusor001 <linus.ji.001@gmail.com>
2021-05-31 15:46:09 -0700

    after delete, and git ignore added

--- test/moduleCombine.js
+++ test/moduleCombine.js
@@ -1,10 +1,10 @@
-// const tail = require('../tail')
-// const eqArrays = require('../eqArrays')
-// const assertArraysEqual = require('../assertArraysEqual')
-// const middle = require('../middle')
+const tail = require('../tail')
+const eqArrays = require('../eqArrays')
+const assertArraysEqual = require('../assertArraysEqual')
+const middle = require('../middle')
 
-// // TEST CODE
-// console.log(eqArrays([1, 2, 3], [1, 2, 'a']))
-// console.log(tail([1,2,3,4]))
-// console.log(assertArraysEqual([1, 2, 3], [1, 2, 'a']))
-// console.log(middle([1,2,3,4,5]), middle([1,2,3,4,5,6]), middle([1]))
+// TEST CODE
+console.log(eqArrays([1, 2, 3], [1, 2, 'a']))
+console.log(tail([1,2,3,4]))
+console.log(assertArraysEqual([1, 2, 3], [1, 2, 'a']))
+console.log(middle([1,2,3,4,5]), middle([1,2,3,4,5,6]), middle([1]))
